9a002769f241fa73e83fffcbe0837e2318ce9ce7
matei jordache <mateijordache@live.com>
2026-01-01 14:13:41 -0500

    use pooling

--- src/gerber2ems/importer.py
+++ src/gerber2ems/importer.py
@@ -56,7 +56,8 @@ def process_gbrs_to_pngs() -> None:
     if mask is not None:
         logger.debug("Masking gerbers with ROI")
         mask_png = gbr_to_png(edge, mask)
-        copper_pngs.map(partial(and_with_mask, mask_png), copper_pngs)
+        with Pool(initargs=(cfg._config,), initializer=Config.set_config) as p:
+            p.map(partial(and_with_mask, mask_png), copper_pngs)
 
 def gbr_to_png(edge_filename: Path, gerber_filename: Path) -> None:
     """Generate PNG from gerber file.
